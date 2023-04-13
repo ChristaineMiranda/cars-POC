98b874c1997dbbddd81764a1b991c1d21679242c
christaine <christaine.miranda@gmail.com>
2023-04-13 09:44:20 -0300

    exercicio

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,56 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  const data = await prisma.cars.findMany();
+  return data;
+}
+
+async function getCar(id: number) {
+  const data = await prisma.cars.findUnique({where:{id: id}});
+  return data;
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  const data = await prisma.cars.findUnique({where:{"licensePlate": licensePlate}});
+  return data;
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  await prisma.cars.create({data:{
+    model: model,
+    "licensePlate": licensePlate,
+    year: year,
+    color: color
+  }});
+  }
+
+async function deleteCar(id: number) {
+  await prisma.cars.delete({where:{
+    id: id
+  }});
+}
+
+async function updateCar(id:number, model: string, licensePlate: string, year: number, color: string) {
+  await prisma.cars.update({
+    where: {
+      id:id,
+    },
+    data: {
+        model: model,
+        "licensePlate": licensePlate,
+        year: year,
+        color: color
+  }})
+  
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar,
+  updateCar
+}
+
+export default carRepository;
